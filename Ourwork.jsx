c291ae1b65564c42c633d2dab84fa3e1577fdead
Farrukh Aslam <47663235+lvlFarrukh@users.noreply.github.com>
2024-02-27 19:30:50 +0500

    Add files via upload

---
+++ Ourwork.jsx
@@ -0,0 +1,11 @@
+import SwiSwiperSlider from '@/components/Swiper/SwiperSlider'
+import React, { Fragment } from 'react'
+import './ourwork.css'
+
+export default function Ourwork() {
+  return (
+    <Fragment>
+        
+    </Fragment>
+  )
+}
